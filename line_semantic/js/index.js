812aa8e077b62399987fd9871b5ca2a6d86c63b7
Sub2n <95su1208@gmail.com>
2019-04-10 17:19:56 +0900

    feat: submenu hover event (no transition)

--- line_semantic/js/index.js
+++ line_semantic/js/index.js
@@ -1,28 +1,38 @@
 
 var buttonLang = $('.btn-language');
 var listLang = $('.language-list');
+var menuItem = $('.menu-item');
 
 // language button click event
 buttonLang.on('click keyup', function(e){
     if(e.keyCode === 9){
         listLang.parent().addClass('language-act');
-        console.log('tab');
     }
     else{
         // tab 아니고 click 됐을 떄
         listLang.parent().toggleClass('language-act');
-        console.log('click');
     }
-});
-
-listLang.on('focusout', '.language-item:last-child', function(){
-    listLang.parent().removeClass('language-act');
-    console.log('focus out');
 });
 
 // event capturing & bubbling
+// div.language 외의 영역 click시 닫기
 document.addEventListener("click", function(e) {
+    console.log(e);
     if (!e.path.includes(document.querySelector(".language"))) {
         listLang.parent().removeClass('language-act');
     }
-})
+});
+
+// div.language 외의 영역에 tab key 눌리면 닫기
+document.addEventListener("keyup", function(e){
+    if(e.keyCode === 9){
+        if (!e.path.includes(document.querySelector(".language"))) {
+            listLang.parent().removeClass('language-act');
+        }
+    }
+});
+
+menuItem.on('mouseenter', function(){
+    menuItem.removeClass('menu-act');
+    $(this).addClass('menu-act');
+});
